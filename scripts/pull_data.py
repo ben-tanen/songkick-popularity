d306e04fc544e31b1922502665c8f9243e9c5333
Ben Tanen <ben.tanen@gmail.com>
2018-11-19 22:21:01 -0500

    updated pull_data.py script; pulled in rest of data for analysis

--- scripts/pull_data.py
+++ scripts/pull_data.py
@@ -2,17 +2,15 @@
 # LOAD PACKAGES #
 #################
 
-import requests, re, json
-from datetime import datetime
-from bs4 import BeautifulSoup
+import requests, json
+import pandas as pd
 
 ###############
 # GET API KEY #
 ###############
 
-if __name__ == "__main__":
-    api_keys = json.load(open("data/api-keys.json"))
-    api_key = api_keys['songkick-api-key']
+api_keys = json.load(open("data/api-keys.json"))
+api_key = api_keys['songkick-api-key']
 
 ###########################
 # DEFINE HELPER FUNCTIONS #
@@ -29,6 +27,10 @@ def get_api_data(url):
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
+    
+############################
+# DEFINE PARSING FUNCTIONS #
+############################
 
 def search_artists(query):
     url = "https://api.songkick.com/api/3.0/search/artists.json?apikey=%s&query=%s" % (api_key, query)
@@ -52,8 +54,97 @@ def get_artist_events(artist_id):
 def get_venue(venue_id):
     url = "https://api.songkick.com/api/3.0/venues/%d.json?apikey=%s" % (venue_id, api_key)
     return get_api_data(url)
-    
-############################
-# DEFINE PARSING FUNCTIONS #
-############################
 
+#######################
+# LOOK FOR ARTIST IDS #
+#######################
+
+# pull in artist names without IDs
+artists = pd.read_csv('data/artists.csv', encoding = 'latin1')
+
+for artist_ix in range(0, len(artists['artist'])):
+    artist = artists.iat[artist_ix, 0]
+
+    print(">>> pulling search results for '%s'" % artist)
+
+    # query search results
+    artist_results1 = search_artists(artist)
+    artist_results2 = [{'name': r['displayName'], 'id': r['id']} for \
+                       r in artist_results1['resultsPage']['results']['artist']]
+    
+    # fill in if only one results
+    # else output results to do manually
+    if len(results2) == 1:
+        artists.iat[artist_ix, 1] = results2[0]['id']
+    else:
+        print("=== %s ===" % artist)
+        print(results2)
+
+# stop until ids have been filled in
+input('Press any key once artist IDs filled in...')
+
+##############################
+# PULL IN EVENTS FOR ARTISTS #
+##############################
+
+# pull in artists after filling in IDs
+artists = pd.read_csv('data/artists.csv', encoding = 'latin1')
+
+# loop through artists and pull in full list of events
+all_events = [ ]
+
+for ix, row in artists.iterrows():
+    [artist, artist_id] = [row['artist'], row['id']]
+
+    print(">>> pulling event results for %s" % artist)
+
+    event_results = get_artist_events(artist_id)
+
+    # parse out content for each event
+    for e in event_results:
+        all_events.append({
+            'name': e['displayName'],
+            'id': e['id'],
+            'billing': [a['billing'] for a in e['performance'] if a['artist']['id'] == artist_id][0],
+            'popularity': e['popularity'],
+            'date': e['start']['date'],
+            'venue-id': e['venue']['id'],
+            'venue-name': e['venue']['displayName']
+        })
+
+# convert to df and export
+all_events_df = pd.DataFrame(all_events)
+all_events_df.to_csv('data/concerts.csv', index = False)
+
+#############################
+# PULL IN VENUES FOR EVENTS #
+#############################
+
+# get unique list of venue ids
+venue_ids = all_events_df['venue-id'].unique().tolist()
+
+# loop through venue ids and pull in relevant information
+all_venues = [ ]
+
+for venue_id in venue_ids:
+    try:
+        venue_results = get_venue(venue_id)
+        venue = venue_results['resultsPage']['results']['venue']
+    except:
+        print(">>> %s - invalid venue id" % venue_id)
+        continue
+
+    print(">>> pulling venue results for %s, %d/%d" % (venue['displayName'], venue_ids.index(venue_id), len(venue_ids)))
+
+    all_venues.append({
+        'name': venue['displayName'],
+        'id': venue['id'],
+        'metroArea': venue['metroArea']['displayName'],
+        'lat': venue['lat'],
+        'lng': venue['lng'],
+        'capacity': venue['capacity']
+    })
+
+# convert to df and export
+all_venues_df = pd.DataFrame(all_venues)
+all_venues_df.to_csv('data/venues.csv', index = False)
